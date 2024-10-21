097167469f51b97a22c2698ff57d09c7c561df62
MasterShifuCsgo <kasparbergert6@gmail.com>
2024-10-21 13:25:07 +0300

    Add boxen

--- index.js
+++
@@ -1,8 +0,0 @@
-//const fs = require('fs'); // cmmonjs module
-//import fs from 'fs'; // es module
-//fs.writeFileSync('test txt', 'Hello node!');
-
-const common = require('./common.js')
-common.hello();
-const es = require('./es.js').default;
-es.hello()
