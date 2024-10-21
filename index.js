4f2bf814634c683f971de3bb07b5de8d0cfb41fb
MasterShifuCsgo <kasparbergert6@gmail.com>
2024-10-21 12:53:01 +0300

    es vs commonjs

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const fs = require('fs');
-fs.writeFileSync('test txt', 'Hello node!');
+//const fs = require('fs'); // cmmonjs module
+//import fs from 'fs'; // es module
+//fs.writeFileSync('test txt', 'Hello node!');
+
+const common = require('./common.js')
+common.hello();
+import es from './es.js';
+es.hello()
