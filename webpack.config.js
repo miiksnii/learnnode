44cce0c95b51871c0f72897b0776d9a64e75bbf7
Kaspar Bergert <50704267035@tptlive.ee>
2024-11-11 14:04:18 +0200

    new files. 11.11.2024

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,20 @@ export default {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader"
+          ,"css-loader",
+          {
+            loader: "sass-loader",
+            options:{
+              sassOptions:{
+                quietDeps:true
+              }
+            }
+          }
+        ],
+      },
     ],
   },
   plugins: [
