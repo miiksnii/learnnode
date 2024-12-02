39eecce35cf8a409209f7f128e36b11ef2a814c1
MasterShifuCsgo <kasparbergert6@gmail.com>
2024-12-02 13:43:58 +0200

    columns

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,17 @@ export default async () => {
   let response = await fetch('https://rickandmortyapi.com/api/character');
   let json = await response.json();
   let characters = json.results;
-  console.log(characters);
+  let pages = [];
+  characters.forEach(character => {
+    let page = new  HtmlWebpackPlugin({
+        template: './src/index.njk',
+        filename:  'character_' + character.id + '.html',
+        templateParameters: {
+          character
+        }
+      });
+      pages.push(page);
+    });
 
   return {
     entry: './src/index.js',
@@ -65,6 +75,7 @@ export default async () => {
         filename: 'about.html',
         template: './src/about.njk'
       }),
+      ... pages
     ],
   };
 }
