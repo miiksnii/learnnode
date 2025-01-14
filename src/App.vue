4fe99df614f94a5ddb33a3e149b75aa1e0bee3d7
MasterShifuCsgo <kasparbergert6@gmail.com>
2025-01-14 11:37:15 +0200

    add items example

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,36 @@
 import { ref } from 'vue';
 
 
-let message = ref('Hello TA-23A')
+let message = ref('');
+let items = ref(['Piim','Või','Leib','Kapsas','Jäätis']);
+
+function addItem(){
+    if(message.value.trim() !==``){
+    items.value.push(message.value.trim());
+    }
+    message.value = '';
+}
 
 </script>
 
 <template>
-    <div class="container">
-        <h1>{{ message.split('').reverse().join('') }}</h1>
-        <button class="button is-primary" v-on:click="message = 'Click Clack'">Click me</button>
-        <input class="input" v-model="message">
+    <div class="container mt-2">
+        <div class="field has-addons">
+            <div class="control">
+                <input class="input" type="text" v-model="message" @keypress.enter="addItem">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="addItem">
+                    Add item
+                </button>
+            </div>
+        </div>
+        <div class="content">
+            <h3>All items</h3>
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
+        </div>
     </div>
 </template>
 
