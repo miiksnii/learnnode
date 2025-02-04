fc55a650458126557ed84178b52422d1a9d53bef
MasterShifuCsgo <kasparbergert6@gmail.com>
2025-02-04 11:21:14 +0200

    ImgModal

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,39 @@
 <script setup>
-import {ref} from 'vue';
+import { ref } from 'vue';
+import Modal from './components/Modal.vue';
+import ImgModal from './components/ImgModal.vue';
+
 
 let modalActive = ref(false);
-
-document.body.addEventListener('keypress', event =>{
-    console.log(event);
-    if(event.key === 'Escape'){
-        modalActive.value = false;
-    }
-});
+let modalActive2 = ref(false);
+let modalActive3 = ref(false);
 
 </script>
 
 <template>
-    <div class="container" @keypress.esc="pressed">
+    <div class="container">
         <section class="section">
-            <button class="button is-primary" @click="modalActive=true"> Modal Active</button>
+            <div class="buttons">
+                <button class="button is-primary" @click="modalActive = true"> Modal Active</button>
+                <button class="button is-warning is-outlined ml-2" @click="modalActive2 = true"> Modal 2 Active</button>
+                <button class="button is-danger is-rounded" @click="modalActive3 = true"> Modal Active3</button>
+            </div>
         </section>
     </div>
 
-<div class="modal" :class="{'is-active': modalActive}">
-  <div class="modal-background" @click="modalActive=false"></div>
-  <div class="modal-content">
-    <p class="image is-4by3">
-      <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="">
-    </p>
-  </div>
-  <button class="modal-close is-large" @click="modalActive=false" aria-label="close"></button>
-</div>
+    <ImgModal :active="modalActive" @close="modalActive = false" url="https://picsum.photos/200/300?random=1">
+    </ImgModal>
+    <ImgModal :active="modalActive2" @close="modalActive2 = false" url="https://picsum.photos/200/300?random=2">
+    </ImgModal>
+    <Modal :active="modalActive3" @close="modalActive3 = false">
+        <div class="notification is-link">
+            <button class="delete"></button>
+            Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum
+            dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta
+            nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus
+            diam, et dictum <a>felis venenatis</a> efficitur.
+        </div>
+    </Modal>
 
 </template>
 
