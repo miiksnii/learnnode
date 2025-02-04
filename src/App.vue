fd063d1f680df95c6e579bf94c06f604a83b092b
MasterShifuCsgo <kasparbergert6@gmail.com>
2025-02-04 11:39:43 +0200

    Add Tabs component

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,12 @@
 <script setup>
-import { ref } from 'vue';
-import Modal from './components/Modal.vue';
-import ImgModal from './components/ImgModal.vue';
-
-
-let modalActive = ref(false);
-let modalActive2 = ref(false);
-let modalActive3 = ref(false);
+import {ref} from 'vue';
+import Tabs from './components/Tabs.vue';
+let titles = ref(['Item1', 'Item2', 'Item3', 'Item4']);
 
 </script>
 
 <template>
-    <div class="container">
-        <section class="section">
-            <div class="buttons">
-                <button class="button is-primary" @click="modalActive = true"> Modal Active</button>
-                <button class="button is-warning is-outlined ml-2" @click="modalActive2 = true"> Modal 2 Active</button>
-                <button class="button is-danger is-rounded" @click="modalActive3 = true"> Modal Active3</button>
-            </div>
-        </section>
-    </div>
-
-    <ImgModal :active="modalActive" @close="modalActive = false" url="https://picsum.photos/200/300?random=1">
-    </ImgModal>
-    <ImgModal :active="modalActive2" @close="modalActive2 = false" url="https://picsum.photos/200/300?random=2">
-    </ImgModal>
-    <Modal :active="modalActive3" @close="modalActive3 = false">
-        <div class="notification is-link">
-            <button class="delete"></button>
-            Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum
-            dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta
-            nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus
-            diam, et dictum <a>felis venenatis</a> efficitur.
-        </div>
-    </Modal>
-
+    <Tabs :items="titles"></Tabs>
 </template>
 
 <style></style>
