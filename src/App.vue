7df0ef2cb4a8b3b576dfe2ccffc8f8d352ff4e18
Kaspar Bergert <50704267035@tptlive.ee>
2025-03-18 11:45:36 +0200

    _General_-20250204_100523 time-stamp 33:21

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 
 import {ref} from 'vue';
-import Modal from './components'
+import Modal from './components/Modal.vue';
+
+
 
 let modalActive = ref(false);
+let modalActive2 = ref(false);
 
 document.body.addEventListener('keydown', event => {
   console.log(event);
@@ -11,20 +14,18 @@ document.body.addEventListener('keydown', event => {
     emit('close');
   }
 })
-
-
 </script>
-
 <template>
 
 <div class="container">
   <section class="section">
     <button class="button is-primary" @click="modalActive=true">Modal Active</button>
-    <button class="button is-warning is-outlined ml-2" @click="modalActive=true">Modal 2 Active</button>
+    <button class="button is-warning is-outlined ml-2" @click="modalActive2=true">Modal 2 Active</button>
   </section>
 
 </div>
 <Modal :active="modalActive" @close="modalActive=false"></Modal>
+<Modal :active="modalActive2" @close="modalActive2=false"></Modal>
 </template>
 
 <style>
