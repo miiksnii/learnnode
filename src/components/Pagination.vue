3e60abdbf8553b4b754365713900080f0fb4c3a7
MasterShifuCsgo <kasparbergert6@gmail.com>
2025-03-18 11:12:29 +0200

    debounce search

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -5,7 +5,7 @@ let { info, current } = defineProps(['info', 'current']);
 
 let pageNumbers = computed(() => {
     let pages = [];
-    for (let i = 1; i <= 3; i++) {
+    for (let i = 1; i <= 3 && i<=info.pages; i++) {
         pages[i] = i;
     }
     if (current > 2) {
@@ -42,19 +42,11 @@ console.log(pageNumbers.value);
                 aria-current="page"
                 v-else-if="page=== current">{{ page }}</a>
 
-                <a href="#"
+                <a @click="$emit('page', page)"
                 class="pagination-link"
                 :aria-label="`Goto page ${page}`"
                 v-else>{{ page }}</a>
             </li>
-            <!-- <li><span class="pagination-ellipsis">&hellip;</span></li>
-            <li><a href="#" class="pagination-link" aria-label="Goto page 45">45</a></li>
-            <li>
-                <a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a>
-            </li>
-            <li><a href="#" class="pagination-link" aria-label="Goto page 47">47</a></li>
-            <li><span class="pagination-ellipsis">&hellip;</span></li>
-            <li><a href="#" class="pagination-link" aria-label="Goto page 86">86</a></li> -->
         </ul>
     </nav>
 </template>
--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -7,37 +7,73 @@ import Pagination from '../components/Pagination.vue';
 const characters = ref([]);
 const info = ref({});
 const currentPage = ref(1);
+const searchValue = ref('');
+const error = ref('');
+let searchTimeout = null;
+
 
 await getCharacters('https://rickandmortyapi.com/api/character');
 
-async function getCharacters(url) {
-    let responce = await axios.get(url);
-    console.log(responce.data);
-    characters.value = responce.data.results;
-    info.value = responce.data.info;
+async function getCharacters() {
+    try {
+        let responce = await axios.get('https://rickandmortyapi.com/api/character', {
+            params: {
+                page: currentPage.value,
+                name: searchValue.value,
+            }
+        });
+        console.log(responce.data);
+        characters.value = responce.data.results;
+        info.value = responce.data.info;
+    } catch (err) {
+        console.log(err);
+        characters.value = responce.data.results;
+        characters.value = [];
+        info.value = null;
+    }
 }
 
 
 async function next() {
     currentPage.value++;
-    await getCharacters(info.value.next);
+    await getCharacters();
 }
 async function prev() {
     currentPage.value--;
-    await getCharacters(info.value.prev);
+    await getCharacters();
+}
+
+async function page(page) {
+    currentPage.value = page;
+    await getCharacters();
+}
+async function search() {
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(async () => {
+        error.value = '';
+        currentPage.value = 1;
+        await getCharacters();
+    }, 1000);
+
 }
 
 </script>
 <template>
-    <Pagination :info="info" :current="currentPage" @next="next" @prev="prev"></Pagination>
-    <div class="buttons">
-        <button class="button is-primary" @click="prev" :disabled="!info.prev">Prev</button>
-        <button class="button is-primary" @click="next" :disabled="!info.next"> Next</button>
+    <div class="field has-addons">
+        <div class="control is-expanded">
+            <input @input="search" v-model="searchValue" class="input" type="text" placeholder="Find a character">
+        </div>
+        <div class="control">
+            <button @click="search" class="button is-info">
+                Search
+            </button>
+        </div>
     </div>
+    <Pagination v-if="info" :info="info" :current="currentPage" @next="next" @prev="prev" @page="page"></Pagination>
     <div class="columns columns is-multiline">
         <div v-for="character in characters" class="column is-one-quarter">
             <CharacterCard :character="character"></CharacterCard>
         </div>
     </div>
-
+    <h1 v-if="error" class="is-size-1">{{ error }}</h1>
 </template>
