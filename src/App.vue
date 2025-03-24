12bc2f51c2411dc463b9d7bc513c7f2e211e6d5b
Kaspar Bergert <kasparbergert6@gmail.com>
2025-03-24 16:19:40 +0200

    router finished

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@ import { ref } from 'vue';
 import Tabs from './components/Tabs.vue';
 import { RouterView } from 'vue-router';
 
-
+//date 18 2025
+ 
 </script>
 <template>
 <Tabs></Tabs>
 <RouterView></RouterView>
 </template>
+
